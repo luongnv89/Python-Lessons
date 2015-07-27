dfcc774615ff3639a16c36b3109c4a509b73e430
NGUYEN VAN LUONG <luongnv89@gmail.com>
2015-07-27 17:36:41 +0200

    Added Hung exercises

---
+++ hung-homeworks/homework.py
@@ -0,0 +1,86 @@
+import random
+import time
+
+def t(s):
+	for x in range(s):
+		time.sleep(1)
+		print(".",end="",flush=True)
+                
+
+def instro():
+
+	print("Welcome to the dragon world.")
+	
+	print("It has the good dragon and the bad dragon. And it has 2 cave. One for the good dragon. And other is bad dragon...")
+	t(2)
+	print("")
+	
+	print("You are stand in front of 2 cave")
+	t(3)
+	print("")
+	
+	print("You must go into  one of the 2 cave")
+	t(2)
+def choice():
+	print(" ")
+	
+	print("A. First cave   B.Second cave")
+	t(2)
+	print("")
+
+	print("So, choose a cave. You must to be careful...")
+
+	choose=input()
+	
+	return choose
+
+def check(c):
+	
+	print("Are you sureto the "+str(c)+" ? So, let's go!")
+	t(2)
+	
+	print("A dragon step out with a scary sound and...")
+	t(4)
+	print("")
+
+
+	x=random.randint(1,2)
+	# Luong: What is c for? and why assign choose = 1 here?
+	choose=1
+	if choose== 'A':
+		choose=1
+	else:
+		choose=2
+	print("x= "+str(x)+" ? c = "+str(c)+" and choose = "+str(choose))
+	if x==choose:
+		tru()
+	else:
+		fal()	
+def tru():
+
+		print("Lucky.It's the good dragon. You have treasure.Congratulation. See you again.")
+def fal():
+	
+		print("Oh no, it's the bad dragon. The dragon will eat you. See you at the cemetery.") 
+
+#Pr
+def playgame():
+	instro()
+	a=choice()
+	check(a)
+#Start
+playgame()
+
+ 
+print("Do you want to play again? 'yes' or 'no'")
+anwser=input()
+if anwser=='yes':
+	while anwser=='yes':
+		playgame()
+		print("Do you want to play again? 'yes' or 'no'")
+		anwser=input()
+		if anwser=='no':
+			print("See you later")
+else:
+	print("See you later")	
+
